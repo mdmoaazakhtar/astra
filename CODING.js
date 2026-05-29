21534da64b6ee5b9a40634592b7f596876a7f63c
mdmoaazakhtar <mdmoaazakhtar772@gmail.com>
2026-05-29 10:15:56 +0530

    CODING

---
+++ CODING.js
@@ -0,0 +1,82 @@
+const data = [
+  {
+    id: "a",
+    news: "Pak cannot be found since 2 days",
+  },
+
+  {
+    id: "b",
+    news: "Charon keya found on north korean border",
+  },
+
+  {
+    id: "c",
+    news: "From Monday Nepal will be a state of India",
+  },
+
+  {
+    id: "d",
+    news: "New technology launched in India",
+  },
+
+  {
+    id: "e",
+    news: "AI is changing the future",
+  },
+
+  {
+    id: "f",
+    news: "India won the cricket match",
+  },
+
+  {
+    id: "g",
+    news: "Heavy rain expected tomorrow",
+  },
+
+  {
+    id: "h",
+    news: "New metro line started in Delhi",
+  },
+
+  {
+    id: "i",
+    news: "Scientists discovered a new planet",
+  },
+
+  {
+    id: "j",
+    news: "Electric cars are becoming popular",
+  },
+
+  {
+    id: "k",
+    news: "Students preparing for semester exams",
+  },
+
+  {
+    id: "l",
+    news: "Cyber security demand is increasing",
+  },
+
+  {
+    id: "m",
+    news: "SpaceX launched a new rocket",
+  },
+
+  {
+    id: "n",
+    news: "New AI tool released worldwide",
+  },
+
+  {
+    id: "o",
+    news: "Technology is improving daily",
+  },
+];
+
+console.log(data);
+
+console.log(data[0]);
+
+console.log(data[5].news);
