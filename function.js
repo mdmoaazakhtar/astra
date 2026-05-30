d1332440004877a1f38bd2727c410ab9f94738b0
mdmoaazakhtar <mdmoaazakhtar772@gmail.com>
2026-05-30 10:44:44 +0530

    function

---
+++ function.js
@@ -0,0 +1,9 @@
+ function find news(data.id){
+    for(let i=0;i<data length;i++){
+        if(data[i].id===id){
+            return data[i];
+    }
+    }
+    return"news not found";
+}
+console.log(findnews(data,"b"));
